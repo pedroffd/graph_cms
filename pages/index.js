3dc7e5c1534a8521a6a97f4ec028d745ddb78dd9
pedroffd <pedro.alcarin@gmail.com>
2023-10-17 20:55:47 -0300

    data fetched from hygraph

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,35 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  { title: "React testing", excerpt: "learn react testing" },
-  { title: "React with Tailwind", excerpt: "learn react with tailwind" },
-]
+export default function Home({ posts }) {
+    return (
+        <div className="container mx-auto px-10 mb-8">
+            <Head>
+                <title>CMS Blog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                <div className="lg:col-span-8 col-span-1">
+                    {posts.map((post, index) => (
+                        <PostCard post={post.node} key={post.title} />
+                    ))}
+                </div>
+                <div className="lg:col-span-4 col-span-1">
+                    <div className="lg:sticky relative top-8">
+                        <PostWidget />
+                        <Categories />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-export default function Home() {
-  return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => <PostCard post={post} key={post.title} /> )}
-        </div>
-        <div className='lg:col-span-4 col-span-1'>
-        <div className='lg:sticky relative top-8'>
-          <PostWidget />
-        </div>
-        </div>
-      </div>
-    </div>
-  )
+export async function getStaticProps() {
+    const posts = (await getPosts()) || []
+
+    return {
+        props: { posts },
+    }
 }
