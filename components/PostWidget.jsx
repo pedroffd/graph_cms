3ed22c462ee6a013e4f7e9dfb544ccb2eb83639f
pedroffd <pedro.alcarin@gmail.com>
2023-10-18 20:32:05 -0300

    queries to fetch similar and recent posts

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services'
 
-const PostWidget = () => {
+const PostWidget = ({ categories, slug }) => {
+  useState [relatedPosts, setRelatedPosts] = useState([])
+
+  useEffect(() => {
+    if(slug) {
+      getSimilarPosts(category, slug)
+        .then((result) => setRelatedPosts(result))
+    } else {
+      getRecentPosts()
+        .then((result) => setRelatedPosts(result))
+    }
+  
+    },[input])
+  
+  
   return (
     <div>
       PostWidget
