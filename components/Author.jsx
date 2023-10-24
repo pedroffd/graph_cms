4cad6d201287917d1c3151942c9120bb2f4ddbdb
pedroffd <pedro.alcarin@gmail.com>
2023-10-24 18:45:04 -0300

    comments form first draft

--- components/Author.jsx
+++ components/Author.jsx
@@ -5,8 +5,9 @@ const Author = ({ author }) => {
   return (
     <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
       <div className='absolute left-0 right-0 -top-14'>
-      <img 
+      <Image 
           alt={author.name}
+          unoptimized
           height='100px'
           width='100px'
           className='align-middle rounded-full'
--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
+  const [error, setError ] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const commentEl = useRef()
+  const nameEl = useRef()
+  const emailEl = useRef()
+  const storeDataEl = useRef()
+
   return (
     <div>
       <h1>CommentsForm</h1>
