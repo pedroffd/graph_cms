a9e510efc725bc8a75d17f32a8e59e848c444ba8
pedroffd <pedro.alcarin@gmail.com>
2023-10-25 15:52:53 -0300

    updated comments form component

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -9,9 +9,47 @@ const CommentsForm = ({ slug }) => {
   const emailEl = useRef()
   const storeDataEl = useRef()
 
+  const handleCommentSubmission = () => {
+
+  }
   return (
-    <div>
-      <h1>CommentsForm</h1>
+    <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>comments form</h3>
+      <div className='grid grid-cols-1 gap-4 mb-4'>
+        <textarea 
+          ref={commentEl} 
+          className='p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
+          placeholder='Comment'
+          name='comment'
+        />
+      </div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
+        <input 
+          ref={nameEl}
+          className='py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
+          placeholder='Name'
+          name='name'
+         />
+          <input 
+          ref={emailEl}
+          className='py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
+          placeholder='Email'
+          name='email'
+         />
+      </div>
+      {error && <p className='text-xs text-red-500'>All fields are required.</p>}
+      <div className='mt-8'>
+        <button 
+          type='button' 
+          onClick={handleCommentSubmission}
+          className='trasition duration-500 ease hover:bg-indigo-800 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer'
+
+          >
+            Post comment
+
+
+        </button>
+      </div>
     </div>
   )
 }
