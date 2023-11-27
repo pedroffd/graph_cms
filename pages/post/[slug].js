1709f48723fad3dd1f1691f169ebbf5515db7582
pedroffd <pedro.alcarin@gmail.com>
2023-11-27 20:15:43 -0300

    loader for post page

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -7,9 +7,16 @@ import {
     Author,
     Comments,
     CommentForm,
+    Loader,
 } from '../../components'
+import { useRouter } from 'next/router'
 
 const PostDetails = ({ post }) => {
+    const router = useRouter()
+
+    if (router.isFallback) {
+        return <Loader />
+    }
     return (
         <div className="container mx-auto px-10 mb-8">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -50,6 +57,6 @@ export async function getStaticPaths() {
 
     return {
         paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-        fallback: false,
+        fallback: true,
     }
 }
